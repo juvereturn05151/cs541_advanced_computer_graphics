5e73e8e47edd4a128f7f9f8d1e3862b074089b76
juvereturn@gmail.com <Rusty05151>
2024-12-10 11:24:58 -0800

    Create HDR class

--- CS541-framework/HDR.h
+++ CS541-framework/HDR.h
@@ -1,4 +1,21 @@
 #pragma once
+
+#ifndef _HDR
+#define _HDR
+
+#include<string>
+
 class HDR
 {
+public:
+    unsigned int textureId;
+    int width, height, depth;
+    float* image;
+    HDR();
+    HDR(const std::string& filename);
+
+    void BindTexture(const int unit, const int programId, const std::string& name);
+    void UnbindTexture(const int unit);
 };
+
+#endif
